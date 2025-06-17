dbd87794470ee5285ee097f53a03ac9a4d0dac67
Leonid Pilyugin <l.pilyugin04@gmail.com>
2025-06-17 17:06:25 +0300

    argument checks and readme

--- README.md
+++ README.md
@@ -4,13 +4,35 @@ Shitpost FFmpeg-powered command line tool
 # Requirements
 FFmemepeg requires FFmpeg and Impact font.
 
+# Examples
+Example assets can be found in `example/` directory.
+
+<img src="example/osaka.png" width=400>
+
+```sh
+./ffmemepeg.sh -t "TOP TEXT" -b "BOTTOM TEXT" -i example/osaka.png
+```
+
+<img src="example/out.png" width=400>
+
+FFmemepeg can also manipulate GIFes and videos.
+
+<img src="example/konata.gif" width=400>
+
+```sh
+./ffmemepeg.sh -t "TOP TEXT" -b "BOTTOM TEXT" -i example/konata.gif
+```
+
+<img src="example/out.gif" width=400>
+
 # Usage
+
+Print help message using `-h` option.
 ```sh
 $ ./ffmemepeg.sh -h
 ffmemepeg.sh [OPTION]... -i INPUT
 
 Options:
-    -h, --help              Display this help and exit
     -t, --top               Top text
     -b, --bot               Bottom text
     -o, --output            Output path
@@ -19,26 +41,55 @@ Options:
     -m, --bottom-margin     Bottom margin
     -S, --top-scale         Top text fontsize scale
     -s, --bottom-scale      Bottom text fontsize scale
+    -h, --help              Display this help and exit
     -d, --debug             Show debug info
 ```
 
-# Examples
-Example assets can be found in `example/` directory.
+The text, putting on image is splitted on two blocks: top text and bottom text.
+To add line to top block, use `-t` option.
 
-<img src="example/osaka.png" width=400>
+```sh
+./ffmemepeg.sh -i example/osaka.png -t "TOP TEXT"
+```
+
+<img src="example/1.png" width=400>
+
+You can add several lines of text.
+
+```sh
+./ffmemepeg.sh -i example/osaka.png -t "TOP TOP TEXT" -t "BOTTOM TOP TEXT"
+```
+
+<img src="example/2.png" width=400>
+
+For bottom text, use `-b` option.
 
 ```sh
 ./ffmemepeg.sh -i example/osaka.png -t "TOP TEXT" -b "BOTTOM TEXT"
 ```
 
-<img src="example/out.png" width=400>
+<img src="example/3.png" width=400>
 
-FFmemepeg can also manipulate GIFes and videos
-
-<img src="example/konata.gif" width=400>
-
+Top and bottom blocks have their own font sizes and margins.
 ```sh
-./ffmemepeg.sh -i example/konata.gif -t "TOP TEXT" -b "BOTTOM TEXT"
+./ffmemepeg.sh -i example/osaka.png -t "TOP TOP TEXT" -t "BOTTOM TOP TEXT" -b "TOP BOTTOM TEXT" -b "BOTTOM BOTTOM TEXT"
 ```
 
-<img src="example/out.gif" width=400>
+<img src="example/4.png" width=400>
+
+You can change font size using `-S` option for top block and `-s` for bottom. The font of whole block will be changed.
+The argument of size option is a reliative increase ratio: passing `-S 1.2` and `-s 0.7` options means increasing top block
+font size by 20% and decrease bottom block font size by 30%
+```sh
+./ffmemepeg.sh -i example/osaka.png -t "TOP TOP TEXT" -t "BOTTOM TOP TEXT" -b "TOP BOTTOM TEXT" -b "BOTTOM BOTTOM TEXT" -S 1.2 -s 0.7
+```
+
+<img src="example/5.png" width=400>
+
+Blocks also have margins. They can be changed using `-M` and `-m` options. Their size is measured
+reliative to image height. The default margins are 0.05 for both top and bottom blocks.
+```sh
+./ffmemepeg.sh -i example/osaka.png -t "TOP TOP TEXT" -t "BOTTOM TOP TEXT" -b "TOP BOTTOM TEXT" -b "BOTTOM BOTTOM TEXT" -M 0 -m 0.2
+```
+
+<img src="example/6.png" width=400>
--- ffmemepeg.sh
+++ ffmemepeg.sh
@@ -3,7 +3,6 @@
 USAGE="$(basename "$0") [OPTION]... -i INPUT
 
 Options:
-    -h, --help              Display this help and exit
     -t, --top               Top text
     -b, --bot               Bottom text
     -o, --output            Output path
@@ -12,6 +11,7 @@ Options:
     -m, --bottom-margin     Bottom margin
     -S, --top-scale         Top text fontsize scale
     -s, --bottom-scale      Bottom text fontsize scale
+    -h, --help              Display this help and exit
     -d, --debug             Show debug info
 "
 
@@ -27,25 +27,34 @@ fi
 
 # Parse arguments
 OUTPUT="out"
-TOP_TEXT=""
-BOT_TEXT=""
+TOP_TEXT=()
+BOT_TEXT=()
 TOP_MARGIN=0.05
-BOTTOM_MARGIN=0.05
+BOT_MARGIN=0.05
 TOP_SCALE=1.0
 BOT_SCALE=1.0
+INTERLINE=0.1
 
 VALID_ARGS=$(getopt -o t:b:o:i:M:m:S:s:hd --long top:,bottom:,output:,input:,top-margin:,bottom-margin:,top-scale:,bottom-scale:,help,debug, -- "$@")
 
 eval set -- "$VALID_ARGS"
 
+function check_float () {
+    grep -qE '^[0-9]+(\.[0-9]+)?$' <<<"$1";
+}
+
+function check_text () {
+    grep -v "'" --quiet <<<"$1"
+}
+
 while :;do
     case $1 in
         -t | --top)
-            TOP_TEXT="$2"
+            TOP_TEXT+=("$2")
             shift 2
             ;;
         -b | --bottom)
-            BOT_TEXT="$2"
+            BOT_TEXT+=("$2")
             shift 2
             ;;
         -o | --output)
@@ -61,7 +70,7 @@ while :;do
             shift 2
             ;;
         -m | --bottom-margin)
-            TOP_MARGIN="$2"
+            BOT_MARGIN="$2"
             shift 2
             ;;
         -S | --top-scale)
@@ -87,7 +96,40 @@ while :;do
     esac
 done
 
-# Check if input is provided
+# Check if margins are float
+if ! check_float "$TOP_MARGIN"; then
+    echo "Top margin should be a float number, not \"$TOP_MARGIN\"" >&2
+    exit 1
+fi
+if ! check_float "$BOT_MARGIN"; then
+    echo "Bottom margin should be a float number, not \"$BOT_MARGIN\"" >&2
+    exit 1
+fi
+# Check if scales are float
+if ! check_float "$TOP_SCALE"; then
+    echo "Top scale should be a float number, not \"$TOP_SCALE\"" >&2
+    exit 1
+fi
+if ! check_float "$BOT_SCALE"; then
+    echo "Bottom scale should be a float number, not \"$BOT_SCALE\"" >&2
+    exit 1
+fi
+
+# Check if text does not contain ' symbol
+for PHRASE in "${TOP_TEXT[@]}"; do
+    if ! check_text "$PHRASE"; then
+        echo "Phrase \"$PHRASE\" contains ' symbol. Remove it" >&2
+        exit 1
+    fi
+done
+for PHRASE in "${BOT_TEXT[@]}"; do
+    if ! check_text "$PHRASE"; then
+        echo "Phrase \"$PHRASE\" contains ' symbol. Remove it" >&2
+        exit 1
+    fi
+done
+
+# Check if input was provided
 if [ -z "$INPUT" ]; then
     echo "Input file was not provided. Use -i path" >&2
     exit 1
@@ -99,31 +141,76 @@ if [ ! -f "$INPUT" ]; then
     exit 1
 fi
 
-IMAGE_WIDTH=$(ffprobe -v error -select_streams v -show_entries stream=width -of csv=p=0:s=x "$INPUT")
-IMAGE_HEIGHT=$(ffprobe -v error -select_streams v -show_entries stream=height -of csv=p=0:s=x "$INPUT")
-
-IMAGE_MIN=$((IMAGE_WIDTH > IMAGE_HEIGHT ? IMAGE_HEIGHT : IMAGE_WIDTH))
-
-TOP_TEXT_SIZE=$((IMAGE_MIN * 3 / 2 / (${#TOP_TEXT} + 1)))
-TOP_TEXT_SIZE=$(printf %.0f "$(echo "$TOP_TEXT_SIZE * $TOP_SCALE" | bc)")
-
-BOT_TEXT_SIZE=$((IMAGE_MIN * 3 / 2 / (${#BOT_TEXT} + 1)))
-BOT_TEXT_SIZE=$(printf %.0f "$(echo "$BOT_TEXT_SIZE * $BOT_SCALE" | bc)")
-
 # Check if ffmpeg can read input file
 ffmpeg -i "$INPUT" -f null /dev/null &>/dev/null || echo "ffmpeg cannot read input file \"$INPUT\"" >&2
 
-# Build top and bottom text arguments
+# Get image sizes
+IMAGE_WIDTH=$(ffprobe -v error -select_streams v -show_entries stream=width -of csv=p=0:s=x "$INPUT")
+IMAGE_HEIGHT=$(ffprobe -v error -select_streams v -show_entries stream=height -of csv=p=0:s=x "$INPUT")
+IMAGE_MIN=$((IMAGE_WIDTH > IMAGE_HEIGHT ? IMAGE_HEIGHT : IMAGE_WIDTH))
+
 TEXT_TEMPLATE="drawtext=font=Impact:fontcolor=white:bordercolor=black:x=(w-text_w)/2"
 
-TOP_TEXT_ARGUMENT="$TEXT_TEMPLATE:fontsize=$TOP_TEXT_SIZE:borderw=$((TOP_TEXT_SIZE/20)):text='$TOP_TEXT':y=$TOP_MARGIN*h"
-BOT_TEXT_ARGUMENT="$TEXT_TEMPLATE:fontsize=$BOT_TEXT_SIZE:borderw=$((BOT_TEXT_SIZE/20)):text='$BOT_TEXT':y=h-text_h-$BOTTOM_MARGIN*h"
+# Construct top text argument
+ARGUMENT=""
+K=0
+if [ "${#TOP_TEXT[@]}" -gt 0 ]; then
+    TOP_TEXT_SIZE=$((IMAGE_MIN * 3 / 2 / (${#TOP_TEXT[0]} + 1)))
+    TOP_TEXT_SIZE=$(printf %.0f "$(echo "$TOP_TEXT_SIZE * $TOP_SCALE" | bc)")
+    for PHRASE in "${TOP_TEXT[@]}"; do
+        TEMP_SIZE=$((IMAGE_MIN * 3 / 2 / (${#PHRASE} + 1)))
+        TEMP_SIZE=$(printf %.0f "$(echo "$TEMP_SIZE * $TOP_SCALE" | bc)")
+        if [ "$TOP_TEXT_SIZE" -gt "$TEMP_SIZE" ]; then
+            TOP_TEXT_SIZE="$TEMP_SIZE"
+        fi
+    done
+    N=0
+    for PHRASE in "${TOP_TEXT[@]}"; do
+        ARGUMENT="$ARGUMENT;[$K]$TEXT_TEMPLATE:fontsize=$TOP_TEXT_SIZE:borderw=$((TOP_TEXT_SIZE/20)):text='$PHRASE':y=$TOP_MARGIN*h+$N*$TOP_TEXT_SIZE*(1+$INTERLINE)[$((K+1))]"
+        N=$((N+1))
+        K=$((K+1))
+    done
+    ARGUMENT="${ARGUMENT:1}"
+fi
+
+if [ "${#BOT_TEXT[@]}" -gt 0 ]; then
+    BOT_TEXT_SIZE=$((IMAGE_MIN * 3 / 2 / (${#BOT_TEXT[0]} + 1)))
+    BOT_TEXT_SIZE=$(printf %.0f "$(echo "$BOT_TEXT_SIZE * $BOT_SCALE" | bc)")
+    for PHRASE in "${BOT_TEXT[@]}"; do
+        TEMP_SIZE=$((IMAGE_MIN * 3 / 2 / (${#PHRASE} + 1)))
+        TEMP_SIZE=$(printf %.0f "$(echo "$TEMP_SIZE * $BOT_SCALE" | bc)")
+        if [ "$BOT_TEXT_SIZE" -gt "$TEMP_SIZE" ]; then
+            BOT_TEXT_SIZE="$TEMP_SIZE"
+        fi
+    done
+    N="${#BOT_TEXT[@]}"
+    BARGUMENT=""
+    for PHRASE in "${BOT_TEXT[@]}"; do
+        BARGUMENT="$BARGUMENT;[$K]$TEXT_TEMPLATE:fontsize=$BOT_TEXT_SIZE:borderw=$((BOT_TEXT_SIZE/20)):text='$PHRASE':y=h-$BOT_MARGIN*h-$N*(text_h+$INTERLINE*$BOT_TEXT_SIZE)[$((K+1))]"
+        N=$((N-1))
+        K=$((K+1))
+    done
+    BARGUMENT="${BARGUMENT:1}"
+
+    if [ -n "$ARGUMENT" ]; then
+        ARGUMENT="$ARGUMENT;$BARGUMENT"
+    else
+        ARGUMENT="$BARGUMENT"
+    fi
+fi
+
+if [ -n "$ARGUMENT" ]; then
+    LAST="[$K]"
+    TRUNC_LAST=-${#LAST}
+    ARGUMENT="${ARGUMENT::TRUNC_LAST}"
+    ARGUMENT="-filter_complex \"$ARGUMENT\""
+fi
 
 INPUT_FILENAME=$(basename -- "$INPUT")
 OUTPUT="$OUTPUT.${INPUT_FILENAME##*.}"
 
 # Build ffmpeg command
-FFMPEG_COMMAND="yes | ffmpeg -i \"$INPUT\" -filter_complex \"[0]$TOP_TEXT_ARGUMENT[top];[top]$BOT_TEXT_ARGUMENT\""
+FFMPEG_COMMAND="yes | ffmpeg -i \"$INPUT\" $ARGUMENT"
 FFMPEG_COMMAND="$FFMPEG_COMMAND \"$OUTPUT\""
 
 if [ -n "$DEBUG" ]; then
